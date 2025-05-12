30134f2b1a0d36dec03c7f295848e2a8990e342c
Jônatas <jonatasthome04@gmail.com>
2025-05-11 23:27:14 -0300

    Paper buttom finished

--- js/script.js
+++ js/script.js
@@ -11,6 +11,15 @@ paperBtn.addEventListener("click", function(){
     const randomScore = scores[Math.floor(Math.random() * scores.length)]
     playerChoice.innerText = "Your choice: Paper"
     cpuChoice.innerText = `Computer's choice: ${randomScore}`
+    if (randomScore == scores.at(0)){
+        result.innerText = "Draw Game!"
+    }
+    if (randomScore == scores.at(1)){
+        result.innerText = "You win!"
+    }
+    if (randomScore == scores.at(2)){
+        result.innerText = "You Lose!"
+    }
 })
 
 scissorBtn.addEventListener("click", function(){
@@ -24,3 +33,5 @@ rockBtn.addEventListener("click", function(){
     playerChoice.innerText = "Your choice: Rock"
     cpuChoice.innerText = `Computer's choice: ${randomScore}`
 })
+
+
