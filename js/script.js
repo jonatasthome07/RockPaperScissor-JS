82c9c53af2daf9bedeeb38e04ea1f19afa202da8
Jônatas <jonatasthome04@gmail.com>
2025-05-11 21:46:01 -0300

    Added id in buttons

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const scoreGame = document.querySelector("#score")
+const playerChoice = document.querySelector("#player-choice")
+const cpuChoice = document.querySelector("#cpu-choice")
+const result = document.querySelector("#result")
+
