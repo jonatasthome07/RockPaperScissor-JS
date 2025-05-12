3820e8ae4191f43659a5aafb83d157999aced1f0
Jônatas <jonatasthome04@gmail.com>
2025-05-11 22:21:46 -0300

    Buttons and text sicronized

--- js/script.js
+++ js/script.js
@@ -5,3 +5,15 @@ const result = document.querySelector("#result")
 const rockBtn = document.querySelector("#rock-btn")
 const paperBtn = document.querySelector("#paper-btn")
 const scissorBtn = document.querySelector("#scissor-btn")
+
+paperBtn.addEventListener("click", function(){
+    playerChoice.innerText = "Your choice: Paper"
+})
+
+scissorBtn.addEventListener("click", function(){
+    playerChoice.innerText = "Your choice: Scissor"
+})
+
+rockBtn.addEventListener("click", function(){
+    playerChoice.innerText = "Your choice: Rock"
+})
