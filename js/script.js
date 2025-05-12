e26f075e1ea2af4ebfc7e06cb7fa0e79a80738a8
Jônatas <jonatasthome04@gmail.com>
2025-05-11 21:52:52 -0300

    Rules buttom and query selector in js

--- js/script.js
+++ js/script.js
@@ -2,4 +2,7 @@ const scoreGame = document.querySelector("#score")
 const playerChoice = document.querySelector("#player-choice")
 const cpuChoice = document.querySelector("#cpu-choice")
 const result = document.querySelector("#result")
-
+const rockBtn = document.querySelector("#rock-btn")
+const paperBtn = document.querySelector("#paper-btn")
+const scissorBtn = document.querySelector("#scissor-btn")
+const rulesBtn = document.querySelector("#rules-btn")
