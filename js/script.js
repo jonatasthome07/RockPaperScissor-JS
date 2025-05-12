eb63fa9dbecf1b791d9e7930f44c2095d335285a
Jônatas <jonatasthome04@gmail.com>
2025-05-11 22:53:25 -0300

    Computer's choice added in JS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+const scores = ["Paper", "Rock", "Scissor"];
 const scoreGame = document.querySelector("#score")
 const playerChoice = document.querySelector("#player-choice")
 const cpuChoice = document.querySelector("#cpu-choice")
@@ -7,13 +8,19 @@ const paperBtn = document.querySelector("#paper-btn")
 const scissorBtn = document.querySelector("#scissor-btn")
 
 paperBtn.addEventListener("click", function(){
+    const randomScore = scores[Math.floor(Math.random() * scores.length)]
     playerChoice.innerText = "Your choice: Paper"
+    cpuChoice.innerText = `Computer's choice: ${randomScore}`
 })
 
 scissorBtn.addEventListener("click", function(){
+    const randomScore = scores[Math.floor(Math.random() * scores.length)]
     playerChoice.innerText = "Your choice: Scissor"
+    cpuChoice.innerText = `Computer's choice: ${randomScore}`
 })
 
 rockBtn.addEventListener("click", function(){
+    const randomScore = scores[Math.floor(Math.random() * scores.length)]
     playerChoice.innerText = "Your choice: Rock"
+    cpuChoice.innerText = `Computer's choice: ${randomScore}`
 })
